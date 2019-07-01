fa6199a72fa8ac84499c1491f083ea13bc47b379
Bertho <bertho@pawoon.com>
2019-07-01 16:56:14 +0700

    Import app service

--- app/services/interface/i_item_service.go
+++ app/services/interface/i_item_service.go
@@ -1,5 +1,9 @@
 package _interface
 
+import (
+	"github.com/berthojoris/cart-backend/app/services"
+)
+
 type IItemService interface {
 	services.BaseService
 }
--- app/services/interface/i_order_detail_service.go
+++ app/services/interface/i_order_detail_service.go
@@ -1,5 +1,9 @@
 package _interface
 
+import (
+	"github.com/berthojoris/cart-backend/app/services"
+)
+
 type IOrderDetailService interface {
 	services.BaseService
 }
