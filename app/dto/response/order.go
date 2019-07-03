1afcf8a66849ab6da90a21f1d1ad3eefc1df0f4e
Bertho <bertho@pawoon.com>
2019-07-03 15:01:46 +0700

    Adjust code

--- app/dto/response/order.go
+++ app/dto/response/order.go
@@ -1,7 +1,7 @@
 package response
 
 type Order struct {
-	ID           uint `json:"id"`
-	TotalAmount  uint `json:"total_amount"`
-	OrderDetail OrderDetail
+	ID          uint        `json:"id"`
+	TotalAmount uint        `json:"total_amount"`
+	OrderDetail OrderDetail `json:"detail"`
 }
